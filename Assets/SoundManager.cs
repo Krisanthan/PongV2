e2d47a8519a836b35f1593fecee9c406b2e1eb71
Krisanthan <krish.balakrishnan@mail.utoronto.ca>
2018-01-24 22:58:07 -0500

    goal sound added

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+    public static SoundManager Instance = null;
+
+    public AudioClip goal;
+
+    private AudioSource soundEffectAudio;
 
 	// Use this for initialization
 	void Start () {
-		
+	    
+        if (Instance == null)
+        {
+            Instance = this;
+        } else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        AudioSource[] sources = GetComponents<AudioSource>();
+        
+        foreach(AudioSource source in sources)
+        {
+            if(source.clip = null)
+            {
+                soundEffectAudio = source;
+            }
+        }
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
 }
